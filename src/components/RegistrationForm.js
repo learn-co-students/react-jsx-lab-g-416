b945154f0cb30160a301d616b2955cb95d3f65ac
mikeisfake <mikescooper@icloud.com>
2021-05-07 22:49:59 -0400

    create registration form component

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegistrationForm = () => {
+  retrun (
+    <form>
+      <input type="text"/>
+      <input type="password"/>
+      <button type="submit"></button>
+    </form>
+  )
+}
